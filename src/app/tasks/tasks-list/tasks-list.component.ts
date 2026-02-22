8de1aba74d41e572cc1c3b7a2c37a36b2fc96888
SaravanaKumar SenthilKumar <webdevsaravanan@gmail.com>
2026-02-23 01:20:26 +0530

    Update tasks-list.component.ts

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -27,7 +27,7 @@ export class TasksListComponent {
   openWith(url: string) {
 if (/android/i.test(navigator.userAgent)) {
  url="intent:"+url+"#Intent;action=android.intent.action.VIEW;type=video/*;package="+this.selectedPackageName+";end";
-    window.href=url;
+    window.location.href=url;
 }
 else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
 
