23ac626144e386b84d7deec69debd13d1a4bd276
webdevsaravanan <webdevsaravanan@gmail.com>
2026-02-28 20:54:50 +0530

    add logo design

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/routes' // Import your routes
@@ -8,6 +8,6 @@ import { appRoutes } from './app/routes' // Import your routes
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(appRoutes, withComponentInputBinding()) // Configure the router
+    provideRouter(appRoutes, withComponentInputBinding(),withHashLocation()) // Configure the router
   ]
 }).catch((err) => console.error(err));
